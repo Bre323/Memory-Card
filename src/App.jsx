9c2e7e6bb9ed56b0a4430196aecfdd6b7ca508b1
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2025-11-04 11:44:15 -0300

    Create score component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Logo from './components/logo'
+import Score from './components/score'
 
 function App() {
   return (
     <div className="wrapper">
       <Logo />
+      <Score />
     </div>
   )
 }
---
+++ src/components/score.jsx
@@ -0,0 +1,12 @@
+import '../styles/score.css';
+
+function Score() {
+  return (
+    <div className="score">
+      <p>Personal Record: 0</p>
+      <p>Score: 0</p>
+    </div>
+  )
+}
+
+export default Score;
