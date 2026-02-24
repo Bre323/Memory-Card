51719ae8f8d581a6a4884494fdd762eae6a0b959
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2026-02-24 16:38:31 -0300

    Change index style and set logo style

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,9 +2,9 @@ import { GiBlindfold } from "react-icons/gi";
 
 function Logo() {
   return (
-    <div className="flex gap-10 items-center">
-      <GiBlindfold className="h-40 w-40" />
-      <h1 className="font-size-4xl">Memory Game</h1>
+    <div className="flex gap-5 items-center">
+      <GiBlindfold className="h-20 w-20" />
+      <h1 className="font-bold text-4xl">Memory Game</h1>
     </div>
   )
 }
